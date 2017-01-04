235df1888a774c03c551b46d74c476c6cb970a7f
Christopher Donelson <mr.donelson@Christophers-MacBook-Pro.local>
2017-01-03 19:46:07 -0600

    Beginning of a masterpiece

--- Game Clash/GameViewController.swift
+++ Game Clash/GameViewController.swift
@@ -14,6 +14,11 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let scene = GameScene(fileNamed: "GameScene")
+        let skView = self.view as! SKView
+        scene?.scaleMode = .aspectFit
+        skView.presentScene(scene)
     }
 
 
