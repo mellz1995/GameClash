0d7912f638ba50e549816f347656adcca263fb5a
Christopher Donelson <mr.donelson@christophers-macbook-pro.local>
2017-01-04 21:00:57 -0600

    Tic Tac Toe regular version complete

--- Game Clash/GameViewController.swift
+++ Game Clash/GameViewController.swift
@@ -15,10 +15,21 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = GameScene(fileNamed: "GameScene")
-        let skView = self.view as! SKView
-        scene?.scaleMode = .aspectFit
-        skView.presentScene(scene)
+        if let scene = TicTacFoe(fileNamed: "TicTacFoe"){
+            
+            // Configure the view
+            let skView = self.view as! SKView
+            skView.showsFPS = false
+            skView.showsNodeCount = false
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .aspectFit
+            
+            skView.presentScene(scene)
+        }
     }
 
 
